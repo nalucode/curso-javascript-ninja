4d822f5804c1c204e932a6d59c12372d8573cd0d
Luan Vinícius <40446521+nalucode@users.noreply.github.com>
2019-10-06 19:53:30 -0300

    Update challenge-26.js

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -1,3 +1,5 @@
+(function(){
+    'use strict';
 /*
 O desafio dessa semana é criar uma mini library (biblioteca) para
 reutilizarmos nossos códigos quando fizermos manipulação de DOM!
@@ -19,13 +21,36 @@ selecionados.
 Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
 Só passe para o próximo problema quando tiver resolvido o anterior :)
 */
-// ?
 
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
-  e.preventDefault();
-  console.log('clicou');
-});
+    function DOM(elements){
+        this.element = document.querySelectorAll(elements);
+    }
 
-console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+    DOM.prototype.on = function on(eventType, callback) {
+        Array.prototype.forEach.call(this.element, element => {
+            element.addEventListener(eventType,callback,false);
+        });
+    };
+
+    DOM.prototype.off = function off(eventType, callback){
+        Array.prototype.forEach.call(this.element, element => {
+            element.removeEventListener(eventType,callback,false);
+        });
+    };
+    
+    DOM.prototype.get = function get(){
+        return this.element;
+    };
+
+
+    var $a = new DOM('[data-js="link"]');
+    $a.on('click', function handleCLick(e) {
+        e.preventDefault();
+        console.log('clicou');
+        $a.off('click',handleCLick);
+    });
+
+    console.log('Elementos selecionados:', $a.get());
+    console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+
+}())
